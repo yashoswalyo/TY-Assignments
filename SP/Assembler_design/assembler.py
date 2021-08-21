1fa9c9c90164d8a93b63ff83d2afdfc85b1ad1e6
yashoswalyo <yashoswal18@gmail.com>
2021-08-21 13:48:08 +0530

    tabs instead spaces

--- SP/Assembler_design/assembler.py
+++ SP/Assembler_design/assembler.py
@@ -4,48 +4,48 @@ import sys
 from pyrogram.emoji import MOTOR_BOAT
 
 MOT=[
-['PRINT','IS','01',1],
-['STOP','IS','02',1],
-['ADD','IS','03',1],
-['SUB','IS','04',1],
-['MUL','IS','05',1],
-['MOVER','IS','06',1],
-['MOVEM','IS','07',1],
-['DEC','IS','08',1],
-['DIV','IS','09',1],
-['READ','IS','0A',1],
-['END','AD','01'],
-['START','AD','02'],
-['ORIGIN','AD','03'], #address of next ins
-['EQU','AD','04'],
-['LTORG','AD','05'],    #Assigns address to literals
-['DS','DL','01'],
-['DC','DL','02'],
-['AREG','RG','01'],
-['BREG','RG','02'],
-['CREG','RG','03'],
-['MREG','RG','04'],
-['EQ','CC','01'],
-['LT','CC','02'],
-['GT','CC','03'],
-['LE','CC','04'],
-['GE','CC','05'],
-['NE','CC','06']
+['PRINT','01',1],
+['STOP','02',1],
+['ADD','03',1],
+['SUB','04',1],
+['MUL','05',1],
+['MOVER','06',1],
+['MOVEM','07',1],
+['DEC','08',1],
+['DIV','09',1],
+['READ','0A',1],
+['END','0B'],
+['START','0C'],
+['ORIGIN','0D'],   #address of next ins
+['LTORG','0F'],    #Assigns address to literals
+['DS','10'],
+['DC','11'],
+['AREG','12'],
+['BREG','13'],
+['CREG','14'],
+['MREG','15'],
+['JZ','16'],
+['JNZ','17'],
+['JC','18'],
+['JNC','19']
 ]
 
 
 def pass_one(alp):
-    LC=0
-    length = 20
-    return
+	LC=0
+	length = 20
+	value = 0
+	type = 'none'
+
+	return
 
 
 def getFile():
-    fileName=input("Enter file name: ")
-    alp = open(fileName,'r')
-    return alp
+	fileName=input("Enter file name: ")
+	alp = open(fileName,'r')
+	return alp
 
 if __name__=='__main__':
-    alp=getFile()
-    pass_one(alp)
-    print(MOT[1][2])
+	alp=getFile()
+	pass_one(alp)
+	print(MOT[1][2])
--- SP/Assembler_design/iChecker.py
+++ SP/Assembler_design/iChecker.py
@@ -1,97 +1,97 @@
 #!/bin/python3
 from pyrogram.emoji import *
 class instructionSet(object):
-  NO_OPERAND_INSTRUCTION = [
-      'AAA','AAD','AAM','AAS','CBW'  ,'CLC'  ,'CLD'  ,'CLI'  ,'CMC'  ,'CMPSB' , 'CMPSW'  ,
-      'CWD'  ,'DAA'  ,'DAS'  ,'DIV'  ,'HLT'  ,'INTO'  ,'IRET'  ,'LAHF' ,'LODSB' ,'LODSW' ,
-      'MOVSB' ,'MOVSW' ,'NOP' ,'POPA' ,'POPF' ,'PUSHA' ,'PUSHF' ,'RET' ,'RETF' ,'SAHF' ,
-      'SCASB' ,'SCASW' ,'STC' ,'STD' ,'STI' ,'STOSB' ,'STOSW' ,'XLATB' ,'XCHG' 
-  ]
-  SINGLE_OPERAND_INSTRUCTION = [
-      'CALL','DEC'  ,'IDIV'  ,'IMUL'  ,'INC'  ,'INT'  ,'JA'  ,'JAE'  ,'JB'  ,'JBE'  ,
-      'JC'  ,'JCXZ' , 'JE'  ,'JG'  ,'JGE'  ,'JL'  ,'JLE'  ,'JMP'  ,'JNA'  ,'JNAE'  ,'JNB'  ,'JNBE'  ,
-      'JNC'  ,'JNE'  ,'JNG'  ,'JNGE'  ,'JNL'  ,'JNLE'  ,'JNO'  ,'JNP'  ,'JNS'  ,'JNZ'  ,'JO'  ,'JP'  ,
-      'JPE'  ,'JPO' ,'JS' ,'JZ' ,'LOOP' ,'LOOPE' ,
-      'LOOPNE' ,'LOOPNZ' ,'LOOPZ' ,'MUL' ,'NEG' ,'NOT' ,'POP' ,'PUSH' ,'REP' ,'REPE' ,'REPNE' ,
-      'REPNZ' ,'REPZ' 
-  ]
-  TWO_OPERAND_INSTRUCTION = [
-      'ADC' , 'ADD'  ,'AND'  ,
-      'CMP'  ,
-      'IN' ,'LDS' ,'LEA' ,'LES' ,'MOV' ,'OR' ,'OUT' ,
-      'RCL' ,'RCR' ,'ROL' ,'ROR' ,'SAL' ,'SAR' ,'SBB' ,
-      'SHL' ,'SHR' ,'SUB' ,'TEST' ,'XOR'
-  ]
-  REGISTERS = [
-      'AX', 'BX', 'CX', 'DX', 'AH', 'AL', 'BL', 'BH', 
-      'CH', 'CL', 'DH', 'DL', 'DI', 'SI', 'BP', 'SP'
-  ]
+	NO_OPERAND_INSTRUCTION = [
+		'AAA','AAD','AAM','AAS','CBW'  ,'CLC'  ,'CLD'  ,'CLI'  ,'CMC'  ,'CMPSB' , 'CMPSW'  ,
+		'CWD'  ,'DAA'  ,'DAS'  ,'DIV'  ,'HLT'  ,'INTO'  ,'IRET'  ,'LAHF' ,'LODSB' ,'LODSW' ,
+		'MOVSB' ,'MOVSW' ,'NOP' ,'POPA' ,'POPF' ,'PUSHA' ,'PUSHF' ,'RET' ,'RETF' ,'SAHF' ,
+		'SCASB' ,'SCASW' ,'STC' ,'STD' ,'STI' ,'STOSB' ,'STOSW' ,'XLATB' ,'XCHG' 
+	]
+	SINGLE_OPERAND_INSTRUCTION = [
+		'CALL','DEC'  ,'IDIV'  ,'IMUL'  ,'INC'  ,'INT'  ,'JA'  ,'JAE'  ,'JB'  ,'JBE'  ,
+		'JC'  ,'JCXZ' , 'JE'  ,'JG'  ,'JGE'  ,'JL'  ,'JLE'  ,'JMP'  ,'JNA'  ,'JNAE'  ,'JNB'  ,'JNBE'  ,
+		'JNC'  ,'JNE'  ,'JNG'  ,'JNGE'  ,'JNL'  ,'JNLE'  ,'JNO'  ,'JNP'  ,'JNS'  ,'JNZ'  ,'JO'  ,'JP'  ,
+		'JPE'  ,'JPO' ,'JS' ,'JZ' ,'LOOP' ,'LOOPE' ,
+		'LOOPNE' ,'LOOPNZ' ,'LOOPZ' ,'MUL' ,'NEG' ,'NOT' ,'POP' ,'PUSH' ,'REP' ,'REPE' ,'REPNE' ,
+		'REPNZ' ,'REPZ' 
+	]
+	TWO_OPERAND_INSTRUCTION = [
+		'ADC' , 'ADD'  ,'AND'  ,
+		'CMP'  ,
+		'IN' ,'LDS' ,'LEA' ,'LES' ,'MOV' ,'OR' ,'OUT' ,
+		'RCL' ,'RCR' ,'ROL' ,'ROR' ,'SAL' ,'SAR' ,'SBB' ,
+		'SHL' ,'SHR' ,'SUB' ,'TEST' ,'XOR'
+	]
+	REGISTERS = [
+		'AX', 'BX', 'CX', 'DX', 'AH', 'AL', 'BL', 'BH', 
+		'CH', 'CL', 'DH', 'DL', 'DI', 'SI', 'BP', 'SP'
+	]
 
 if __name__ == '__main__':
-  filename = input("Enter your file name to check(.asm): ")
-  program = open(f'{filename}','r')
-  lines = program.readlines()
-  count = 0
-  for line in lines:
-    count+=1
-    print('--------------------------------------\n')
-    operands = ['-','-']
-    label = "-"
-    instruction = line.strip()
-    print(f"Checking line {count}. .  .   .")
-    instruction = instruction.upper()
+	filename = input("Enter your file name to check(.asm): ")
+	program = open(f'{filename}','r')
+	lines = program.readlines()
+	count = 0
+	for line in lines:
+		count+=1
+		print('--------------------------------------\n')
+		operands = ['-','-']
+		label = "-"
+		instruction = line.strip()
+		print(f"Checking line {count}. .  .   .")
+		instruction = instruction.upper()
 
-    #Checking labels
-    if ": " in instruction:
-      instruction = instruction.split(": ")
-      print(f'Label {CHECK_MARK_BUTTON}')
-      label = instruction[0]
-      instruction = instruction[1]
-      pass
-    else:
-      pass
+		#Checking labels
+		if ": " in instruction:
+			instruction = instruction.split(": ")
+			print(f'Label {CHECK_MARK_BUTTON}')
+			label = instruction[0]
+			instruction = instruction[1]
+			pass
+		else:
+			pass
 
-    #Checking instructions
-    rest = instruction.split(" ")
-    inst = rest[0]
-    if inst in instructionSet.TWO_OPERAND_INSTRUCTION:
-      print(f"Instruction {CHECK_MARK_BUTTON} ")
-      operands = rest[1].split(",")
-      i=1
-      #Checking registers
-      if operands[0]  in instructionSet.REGISTERS:
-        print(f"Register {i} {CHECK_MARK_BUTTON} ")
-        if operands[1] in instructionSet.REGISTERS:
-          i=i+1
-          print(f"Register {i} {CHECK_MARK_BUTTON} ")
-          pass
-        pass
-      elif operands[1] in instructionSet.REGISTERS:
-        print(f"Register {i} {CHECK_MARK_BUTTON} ")
-        
-        pass
-      else:
-        print(f"Register {CROSS_MARK} ")
-        pass
-      pass
+		#Checking instructions
+		rest = instruction.split(" ")
+		inst = rest[0]
+		if inst in instructionSet.TWO_OPERAND_INSTRUCTION:
+			print(f"Instruction {CHECK_MARK_BUTTON} ")
+			operands = rest[1].split(",")
+			i=1
+			#Checking registers
+			if operands[0]  in instructionSet.REGISTERS:
+				print(f"Register {i} {CHECK_MARK_BUTTON} ")
+				if operands[1] in instructionSet.REGISTERS:
+					i=i+1
+					print(f"Register {i} {CHECK_MARK_BUTTON} ")
+					pass
+				pass
+			elif operands[1] in instructionSet.REGISTERS:
+				print(f"Register {i} {CHECK_MARK_BUTTON} ")
+			
+				pass
+			else:
+				print(f"Register {CROSS_MARK} ")
+				pass
+			pass
 
-    elif inst in instructionSet.SINGLE_OPERAND_INSTRUCTION:
-      print(f"Instruction {CHECK_MARK_BUTTON} ")
-      operands[0] = rest[1]
-      i=1
-      if operands[0]  in instructionSet.REGISTERS:
-        print(f"Register {i} {CHECK_MARK_BUTTON} ")
-        pass
-      else:
-        print(f"Register {CROSS_MARK} ")
-        pass
-      pass
+		elif inst in instructionSet.SINGLE_OPERAND_INSTRUCTION:
+			print(f"Instruction {CHECK_MARK_BUTTON} ")
+			operands[0] = rest[1]
+			i=1
+			if operands[0]  in instructionSet.REGISTERS:
+				print(f"Register {i} {CHECK_MARK_BUTTON} ")
+				pass
+			else:
+				print(f"Register {CROSS_MARK} ")
+				pass
+			pass
 
-    elif inst in instructionSet.NO_OPERAND_INSTRUCTION:
-      print(f"Instruction {CHECK_MARK_BUTTON} ")
-      pass
-    else:
-      print(f"Instruction {CROSS_MARK} ")
-      continue
-    print(f"\nLABEL: {label}\nINSTRUCTION: {inst} \nOPERAND 1: {operands[0]}\nOPERAND 2: {operands[1]} ")
-        
+		elif inst in instructionSet.NO_OPERAND_INSTRUCTION:
+			print(f"Instruction {CHECK_MARK_BUTTON} ")
+			pass
+		else:
+			print(f"Instruction {CROSS_MARK} ")
+			continue
+		print(f"\nLABEL: {label}\nINSTRUCTION: {inst} \nOPERAND 1: {operands[0]}\nOPERAND 2: {operands[1]} ")
+			
