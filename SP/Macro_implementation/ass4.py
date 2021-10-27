2defa61ca8d27159091b70b28749d872f13ef5d7
Yash Oswal <yashoswal18@gmail.com>
2021-10-27 19:06:46 +0530

    assignment completed

--- SP/Macro_implementation/ass4.py
+++ SP/Macro_implementation/ass4.py
@@ -1,3 +1,50 @@
+import re
+
+def prototype_processing(prototype,MDTP,KPDTP):
+	KP = 0
+	PP = 0
+	i = 0
+	pwords = [x.upper() for x in prototype ]
+	for i in range(len(pwords)):
+		s=1
+		if pwords[i].startswith('&') and not pwords[i].__contains__('=') :
+			parameter = pwords[i]
+			temp[parameter] = list()
+			temp[parameter].append(f"(P,{str(i)})")
+			PP+=1
+			PNTAB.write(f"{str(pwords[i])}\n")
+		
+		if pwords[i].startswith('&') and pwords[i].__contains__("="):
+			twords = pwords[i].split("=")
+			k_parameter = twords[0]
+			temp[k_parameter] = list()
+			temp[k_parameter].append(f"(P,{str(i)})")
+			if len(twords) == 2 and twords[1] != '':
+				KPDTAB.write(f"{str(KPDTP)} {str(twords[0])} {str(words[0])}")
+				KPDTP += KP
+				KP += 1
+				PNTAB.write(f"{str(twords[0])} \n")
+			else:
+				KP += 1
+				PNTAB.write(f"{str(twords[0])} \n")
+	MNT.write(f"{str(prototype[0])}\t{str(PP)}\t{str(KP)} \t{str(MDTP)} \t{str(KPDTP)}\n")
+
+
+def process_MDT(words):
+	global MDTP
+	for i in words.split():
+		if i == 'MACRO' or i == 'MEND':
+			continue
+		else:
+			if i.__contains__('&') and not i.__contains__('='):
+				var = i.split(',')
+				char = temp[var[0]]
+				MDT.write(f"{char[0]} ")
+			else:
+				MDT.write(f"{i} ")
+	MDTP+=1
+	MDT.write('\n')
+#pointers
 ##ass4 file hehe
 import re
 import os
@@ -23,53 +70,7 @@ lines = macro.readlines()
 mdt = []
 temp = {}
 macroName = []
-#Function To Process Macro Prototype
-def prototype_processing(prototype,MDTP,KPDTP):
-	KP = 0 #Number Of Keyword Parameter
-	PP = 0 #Number Of Positional Parameter
-	i=0
-	macroName = prototype[0]
-	pwords = [x.upper() for x in prototype]
-	for i in range(len(pwords)):
-	#Positional Parameters
-		if pwords[i].startswith('&') and not pwords[i].__contains__('='):
-			parameter = pwords[i]
-			temp[parameter] = list()
-			temp[parameter].append('(P,' + str(i) + ')')
-			PP += 1
-			PNTAB.write(str(pwords[i])+'\n')
-	#Keyword and Default Parameters
-		if pwords[i].startswith('&') and pwords[i].__contains__('='):
-			twords = re.split('=', pwords[i])
-			key_parameter = twords[0]
-			temp[key_parameter] = list()
-			temp[key_parameter].append('(P,' + str(i) + ')')
-			if len(twords) == 2 and twords[1] != '':
-				KPDTAB.write(str(KPDTP) + ' ' + str(twords[0]) + ' ' +str(words[1]))
-				KPDTP += KP
-				KP += 1
-				PNTAB.write(str(twords[0]) + '\n')
-			else:
-				KP += 1
-				PNTAB.write(str(twords[0]) + '\n')
-	MNT.write(str(prototype[0]) + '\t' + str(PP) + '\t' + str(KP) + ' \t' +str(MDTP) + ' \t' + str(KPDTP) + ' \n')
-	prototype_done = True
-#Function To Process MDT
-def process_MDT(words):
-	global MDTP
-	for i in words.split():
-		if i == 'MACRO' or i == 'MEND':
-			continue
-		else:
-			if i.__contains__('&') and not i.__contains__('='):
-				var = i.split(',')
-				char = temp[var[0]]
-				MDT.write(char[0] + ' ')
-			else:
-				MDT.write(i + ' ')
-	MDTP += 1
-	MDT.write('\n')
-#Calculate MNT, PP, KP, KPDTP
+
 i=0
 while i < len(lines):
 	words = re.split(r'[\s,]+', lines[i])
@@ -77,7 +78,7 @@ while i < len(lines):
 	for j in range(len(words)):
 		word = words[j]
 		if word == 'MACRO':
-	#print(words)
+			# print(words)
 			prototype = re.split(r'[\s,]+', lines[i + 1])
 			prototype_processing(prototype, MDTP, KPDTP)
 	process_MDT(lines[i])
