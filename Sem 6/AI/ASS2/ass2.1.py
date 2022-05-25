6b2c2f542a4cb988f62c7455560d57d5d44cc6f1
Yash Oswal <yashoswal18@gmail.com>
2022-05-25 21:50:50 +0530

    wj(alt)

--- Sem 6/AI/ASS2/ass2.1.py
+++ Sem 6/AI/ASS2/ass2.1.py
@@ -1,76 +1,85 @@
-from collections import deque
+'''
+Water-Jug solution using Breadth First Search Algorithm
+'''
 
-def BFS(a, b, target):
-	m = {}
-	isSolvable = False
+print("Solution for water jug problem")
+x_capacity = int(input("Enter Jug 1 capacity:"))
+y_capacity = int(input("Enter Jug 2 capacity:"))
+end = int(input("Enter target volume:"))
+
+def bfs(start, end, x_capacity, y_capacity):
 	path = []
-	
-	q = deque()
-	
-	q.append((0, 0))
+	front = []
+	front.append(start)
+	visited = []
+	#visited.append(start)
+	print(f"{front}\n{visited}\n{path}\n----------------")
 
-	while (len(q) > 0):
+	while(not (not front)):
+		current = front.pop()
+		x = current[0]
+		y = current[1]
+		path.append(current)
+		if x == end or y == end:
+			print("Found!")
+			return path
+		# rule 1
+		if current[0] < x_capacity and ([x_capacity, current[1]] not in visited):
+			front.append([x_capacity, current[1]])
+			visited.append([x_capacity, current[1]])
+
+		# rule 2
+		if current[1] < y_capacity and ([current[0], y_capacity] not in visited):
+			front.append([current[0], y_capacity])
+			visited.append([current[0], y_capacity])
+
+		# rule 3
+		if current[0] > x_capacity and ([0, current[1]] not in visited):
+			front.append([0, current[1]])
+			visited.append([0, current[1]])
+
+		# rule 4
+		if current[1] > y_capacity and ([x_capacity, 0] not in visited):
+			front.append([x_capacity, 0])
+			visited.append([x_capacity, 0])
+
+		# rule 5
+		#(x, y) -> (min(x + y, x_capacity), max(0, x + y - x_capacity)) if y > 0
+		if current[1] > 0 and ([min(x + y, x_capacity), max(0, x + y - x_capacity)] not in visited):
+			front.append([min(x + y, x_capacity), max(0, x + y - x_capacity)])
+			visited.append([min(x + y, x_capacity), max(0, x + y - x_capacity)])
+
+		# rule 6
+		# (x, y) -> (max(0, x + y - y_capacity), min(x + y, y_capacity)) if x > 0
+		if current[0] > 0  and ([max(0, x + y - y_capacity), min(x + y, y_capacity)] not in visited):
+			front.append([max(0, x + y - y_capacity), min(x + y, y_capacity)])
+			visited.append([max(0, x + y - y_capacity), min(x + y, y_capacity)])
+		print(f"{front}\n{visited}\n{path}\n----------------\n----------------")
 		
-		u = q.popleft()
+	return "Not found"
+
+def gcd(a, b):
+	if a == 0:
+		return b
+	return gcd(b%a, a)
+
+# start state: x = 0 , y = 0
+start = [0, 0] 
+#end = 2
+#x_capacity = 4
+#y_capacity = 3
+
+# condition for getting a solution:
+# the target volume 'end' should be a multiple of gcd(a,b)
+
+if end % gcd(x_capacity,y_capacity) == 0:
+	print(bfs(start, end, x_capacity, y_capacity))
+else:
+	print("No solution possible for this combination.")
 
 
-		if ((u[0], u[1]) in m):
-			continue
 
-		if ((u[0] > a or u[1] > b or
-			u[0] < 0 or u[1] < 0)):
-			continue
 
-		path.append([u[0], u[1]])
 
-		m[(u[0], u[1])] = 1
 
-		if (u[0] == target or u[1] == target):
-			isSolvable = True
-			
-			if (u[0] == target):
-				if (u[1] != 0):
-					
-					path.append([u[0], 0])
-			else:
-				if (u[0] != 0):
-
-					path.append([0, u[1]])
-
-			sz = len(path)
-			for i in range(sz):
-				print("(", path[i][0], ",",
-						path[i][1], ")")
-			break
-
-		q.append([u[0], b]) # Fill Jug2
-		q.append([a, u[1]]) # Fill Jug1
-
-		for ap in range(max(a, b) + 1):
-
-			c = u[0] + ap
-			d = u[1] - ap
-
-			if (c == a or (d == 0 and d >= 0)):
-				q.append([c, d])
-
-			c = u[0] - ap
-			d = u[1] + ap
-
-			if ((c == 0 and c >= 0) or d == b):
-				q.append([c, d])
-		
-		q.append([a, 0])
-		
-		q.append([0, b])
-
-	if (not isSolvable):
-		print ("No solution")
-
-if __name__ == '__main__':
-	
-	Jug1, Jug2, target = 4, 3, 2
-	print("Path from initial state to solution state ::")
-	
-	BFS(Jug1, Jug2, target)
 
